e88d7150037906b2a3dceeb73ceb5e3225676387
charanjeetkau <ckbss97@gmail.com>
2020-03-14 11:09:32 +0530

    Added: storing no. of heads/tails in a dictionary and calculating percentage of no. of heads & tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -x
-
+read -p "Enter the number of times you wanna flip a coin: " n
+count=1
+headcount=0
+tailcount=0
+declare -A dictionary
+while [[ $count -le $n ]]
+do
 R=$(( $RANDOM % 10 ))
+	if [[ R -le 5 ]]
+	then
+		res="head"
+		((headcount++))
+	else
+		res="tail"
+		((tailcount++))
+	fi
+dictionary[$count]=$res
+((count++))
+done
+echo ${!dictionary[@]} ' : ' ${dictionary[@]}
+headPercentage=`expr "scale=2;$(($headcount*100))/${#dictionary[@]}" | bc -l`
+tailPercentage=`expr "scale=2;$(($tailcount*100))/${#dictionary[@]}" | bc -l`
+echo "Percentage of Heads: "$headPercentage" %"
+echo "Percentage of Tails: "$tailPercentage" %"
 
-if [[ R -le 5 ]]
-then
-		echo "head"
-else
-		echo "tail"
-fi
