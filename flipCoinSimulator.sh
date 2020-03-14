91e2b73145675d24a1cc05e262a6a56164083c99
charanjeetkau <ckbss97@gmail.com>
2020-03-14 11:41:09 +0530

    Added: storing random combinations in another dictionary and calculating the percentage of each of them

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -23,4 +23,41 @@ headPercentage=`expr "scale=2;$(($headcount*100))/${#dictionary[@]}" | bc -l`
 tailPercentage=`expr "scale=2;$(($tailcount*100))/${#dictionary[@]}" | bc -l`
 echo "Percentage of Heads: "$headPercentage" %"
 echo "Percentage of Tails: "$tailPercentage" %"
-
+counter=1
+hhcount=0
+htcount=0
+thcount=0
+ttcount=0
+declare -A dictionary2
+while [[ $counter -le $n ]]
+do
+R=$(( $RANDOM % 20 ))
+   if [[ R -le 5 ]]
+   then
+      result="HH"
+      ((hhcount++))
+   elif [[ R -gt 5 && R -le 10 ]]
+	then
+      result="HT"
+      ((htcount++))
+	elif [[ R -gt 10 && R -le 15 ]]
+	then
+		result="TH"
+		((thcount++))
+	elif [[ R -gt 15 && R -le 20 ]]
+	then
+		result="TT"
+		((ttcount++))
+   fi
+dictionary2[$counter]=$result
+((counter++))
+done
+echo ${!dictionary2[@]} ' : ' ${dictionary2[@]}
+hhPercentage=`expr "scale=2;$(($hhcount*100))/${#dictionary2[@]}" | bc -l`
+htPercentage=`expr "scale=2;$(($htcount*100))/${#dictionary2[@]}" | bc -l`
+thPercentage=`expr "scale=2;$(($thcount*100))/${#dictionary2[@]}" | bc -l`
+ttPercentage=`expr "scale=2;$(($ttcount*100))/${#dictionary2[@]}" | bc -l`
+echo "Percentage of HH: "$hhPercentage" %"
+echo "Percentage of HT: "$htPercentage" %"
+echo "Percentage of TH: "$thPercentage" %"
+echo "Percentage of TT: "$ttPercentage" %"
